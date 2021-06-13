9cd78d3d6130efa7f752789b1d829a15e4b52bf7
Ashwin Srinath <shwina@users.noreply.github.com>
2021-06-13 16:15:42 -0400

    Some progress -- added a few helpers

--- ielisp.el
+++ ielisp.el
@@ -7,6 +7,21 @@
 
 (setq iel--connection-info (json-read-file (car argv)))
 
+(defun iel--uuidgen ()
+  (replace-regexp-in-string "\n" "" (shell-command-to-string "uuidgen")))
+
+(defun iel--msg-header (msg-type)
+  (json-encode-alist
+   `((session . ,iel--session-id)
+     (msg_id . ,(iel--uuidgen))
+     (date . ,(format-time-string "%FT%T%z"))
+     (msg_type . ,msg-type)
+     (version . "5.0"))))
+
+(defun iel--send (socket msg-type content parent-header metadata identities)
+
+(defun iel--shell-handler (msg)
+  (let* (
 
 (defun iel--bind-addr (port)
   (let* ((transport (cdr (assoc 'transport iel--connection-info)))
@@ -14,6 +29,7 @@
          (port (cdr (assoc-string port iel--connection-info))))
     (format "%s://%s:%s" transport ip port)))
 
+(setq iel--session-id (iel--uuidgen))
 
 (let* ((iel--context (zmq-context))
        (iel--shell-socket (zmq-socket iel--context zmq-ROUTER))
@@ -24,14 +40,16 @@
 
   (zmq-bind iel--shell-socket (iel--bind-addr "shell_port"))
   (zmq-bind iel--iopub-socket (iel--bind-addr "iopub_port"))
-  (zmq-bind iel--stdin-socket (iel--bind-addr "stdin_port"))  
-  (zmq-bind iel--control-socket (iel--bind-addr "control_port"))  
+  (zmq-bind iel--stdin-socket (iel--bind-addr "stdin_port"))
+  (zmq-bind iel--control-socket (iel--bind-addr "control_port"))
   (zmq-bind iel--hb-socket (iel--bind-addr "hb_port"))
 
   (let (msg)
     (while t
       (setq msg (zmq-recv iel--hb-socket))
       (zmq-send iel--hb-socket msg)
-      (sleep-for 1)
-      (message msg)))
+
+      (setq msg (zmq-recv-multipart iel--shell-socket))
+      (message (format "%s"  msg))
+      (message "...")))
 )
