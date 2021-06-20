a9a7143e1ff425eb6ec4bf41c91b7289f0128574
Ashwin Srinath <shwina@users.noreply.github.com>
2021-06-20 19:54:18 -0400

    Document and add interrupt handling

--- ielisp.el
+++ ielisp.el
@@ -1,41 +1,58 @@
-;; ielisp.el
+;;; ielisp.el --- Emacs Lisp kernel for Jupyter
+
+;; Copyright (C) 2021 Ashwin Srinath.
+
+;; Author: Ashwin Srinath
+;; Version: 0.1
+;; Package-Requires: ((zmq "0.10.10"))
+;; Keywords: tools
+;; URL: https://github.com/shwina/ielisp
+
+;;; Commentary:
+
+;; This package provides an Emacs Lisp Kernel for Jupyter
 
 (package-initialize)
-
-(require 'json)
-(require 'zmq)
 (require 'hex-util)
 (require 'hmac-def)
+(require 'json)
+(require 'zmq)
 
-
+;; Read the connection file
 (setq iel--connection-info (json-read-file (car argv)))
 
-;; TODOS:
-;; 1. Document
-;; 2. Installation
-;; 3. Executing code in a different environment
-
 (defun iel--sha256-binary (object)
+  "Return the SHA 256 hash (in binary form) of OBJECT, a buffer or a string."
   (secure-hash 'sha256 object nil nil t))
 
 (define-hmac-function iel--hmac-sha256 iel--sha256-binary 64 32)
 
 (defun iel--hmac-sha256-hex (text key)
-   (encode-hex-string (iel--hmac-sha256 (string-as-unibyte text) key)))
+  "Return the SHA 256 hash (as a hex string) of OBJECT, a buffer or a string."
+  (encode-hex-string (iel--hmac-sha256 (string-as-unibyte text) key)))
 
-;; generate UUID
 (defun iel--uuidgen ()
+  "Generate a UUID using the shell command `uuidgen'."
   (replace-regexp-in-string "\n" "" (shell-command-to-string "uuidgen")))
 
-;; evaluate string and return result as string
-(defun iel--eval-string (code)
-  (format "%s"
-          (condition-case err
-              (eval (car (read-from-string code)))
-            (error (format "%s" (error-message-string err))))))
+(defun iel--handle-interrupt ()
+  "Hook to handle SIGINT"
+  (throw 'iel-interrupt "Interrupted"))
 
-;; generate a message header given a `msg_type`
+(defun iel--eval-string (code)
+  "Evaluate the string CODE and return its value as a string.
+If an error occurs, the corresponding error string is returned"
+  (add-hook 'kill-emacs-hook 'iel--handle-interrupt)
+  (let ((result (catch 'iel-interrupt
+                  (format "%s"
+                          (condition-case err
+                              (eval (car (read-from-string code)))
+                            (error (format "%s" (error-message-string err))))))))
+    (remove-hook  'kill-emacs-hook 'iel--handle-interrupt)
+    result))
+
 (defun iel--msg-header (msg-type)
+  "Generate the message header for a given MSG-TYPE."
    `((session . ,iel--session-id)
      (msg_id . ,(iel--uuidgen))
      (date . ,(format-time-string "%FT%T%z"))
@@ -43,18 +60,19 @@
      (username . "ashwint")
      (version . "5.3")))
 
-;; generate bind address
 (defun iel--bind-addr (port)
+  "Generates the bind address, for the given port name PORT."
   (let* ((transport (cdr (assoc 'transport iel--connection-info)))
          (ip (cdr (assoc 'ip iel--connection-info)))
          (port (cdr (assoc-string port iel--connection-info))))
     (format "%s://%s:%s" transport ip port)))
 
 (defun iel--sign (msg key)
-    (iel--hmac-sha256-hex msg key))
+  "Generate the HMAC signature for MSG, using KEY."
+  (iel--hmac-sha256-hex msg key))
 
-;; construct and send a message on socket
 (defun iel--send (socket msg-type &optional content parent-header metadata identities)
+  "Construct and send a message of type MSG-TYPE on SOCKET."
   (let* ((header (json-encode (iel--msg-header msg-type)))
          (delimiter iel--delimiter)
          (content (if (null content) (json-encode-alist nil) (json-encode content)))
@@ -71,17 +89,22 @@
                                    content))))
     (zmq-send-multipart socket msgs)))
 
-;; deserialize msgs, retuning a list of identities and an alist
-(defun iel--deserialize (msgs)
-  (let* ((delimiter-pos (seq-position msgs iel--delimiter))
-         (identities (seq-subseq msgs 0 delimiter-pos))
-         (frames (seq-subseq msgs (+ 2  delimiter-pos))))
+(defun iel--deserialize (msg-string)
+  "Deserialize MSG-STRING.
+Returns a list whose first element is a list of identities,
+and second element is a mapping whose keys are the names
+of the message dictionaries, and values are the dictionaries
+themselves (alists)."
+  (let* ((delimiter-pos (seq-position msg-string iel--delimiter))
+         (identities (seq-subseq msg-string 0 delimiter-pos))
+         (frames (seq-subseq msg-string (+ 2  delimiter-pos))))
     (list identities `((header  . ,(json-read-from-string (seq-elt frames 0)))
                        (parent_header . ,(json-read-from-string (seq-elt frames 1)))
                        (metadata . ,(json-read-from-string (seq-elt frames 2)))
                        (content . ,(json-read-from-string (seq-elt frames 3)))))))
 
 (defun iel--control-handler (msg)
+  "Handle messages on the control socket."
   (let* ((deserialized-msg (iel--deserialize msg))
          (identities (car deserialized-msg))
          (msg (cadr deserialized-msg))
@@ -90,6 +113,7 @@
       (setq iel--running nil))))
 
 (defun iel--shell-handler (msg)
+  "Handle messages on the shell socket."
   (let* ((deserialized-msg (iel--deserialize msg))
          (identities (car deserialized-msg))
          (msg (cadr deserialized-msg))
@@ -134,10 +158,10 @@
         (iel--send iel--shell-socket "execute_reply" content parent-header nil identities))
       (setq iel--execution-count (+ 1 iel--execution-count)))))
 
-(setq iel--session-id (iel--uuidgen)) ;; per session UUID
-(setq iel--delimiter "<IDS|MSG>") ;; the delimiter between identities and message
-(setq iel--execution-count 1)
-(setq iel--running 't)
+(defconst iel--session-id (iel--uuidgen) "per session UUID")
+(defconst iel--delimiter "<IDS|MSG>")
+(defvar iel--execution-count 1)
+(defvar iel--running 't)
 
 (let* ((iel--context (zmq-context))
        (iel--shell-socket (zmq-socket iel--context zmq-ROUTER))
@@ -172,3 +196,4 @@
         (when shell-events
           (setq msg (zmq-recv-multipart iel--shell-socket))
           (iel--shell-handler msg))))))
+
